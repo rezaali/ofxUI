fc052bd40b4b2c15574eabe848bf5ec3a4af50dd
Reza Ali <syed.reza.ali@gmail.com>
2012-03-05 23:06:38 -0800

    added setMax and setMin for Sliders

---
+++ src/ofxUIMinimalSlider.h
@@ -0,0 +1,14 @@
+//
+//  ofxUIMinimalSlider.h
+//  ofxUIMoreWidgets
+//
+//  Created by Syed Reza Ali on 2/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef ofxUIMoreWidgets_ofxUIMinimalSlider_h
+#define ofxUIMoreWidgets_ofxUIMinimalSlider_h
+
+
+
+#endif
---
+++ src/ofxUIMovingGraph.h
@@ -0,0 +1,14 @@
+//
+//  ofxUIMovingGraph.h
+//  ofxUIMoreWidgets
+//
+//  Created by Syed Reza Ali on 2/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef ofxUIMoreWidgets_ofxUIMovingGraph_h
+#define ofxUIMoreWidgets_ofxUIMovingGraph_h
+
+
+
+#endif
