56f4efe2701c3690400551891f737f2043f5c6ef
Reza Ali <syed.reza.ali@gmail.com>
2012-04-11 13:55:11 -0700

    added functionality to Canvas & new FPS Slider

---
+++ src/ofxUIFPSSlider.h
@@ -0,0 +1,49 @@
+/********************************************************************************** 
+ 
+ Copyright (C) 2012 Syed Reza Ali (www.syedrezaali.com)
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ 
+ **********************************************************************************/
+
+#ifndef OFXUI_FPS_SLIDER
+#define OFXUI_FPS_SLIDER
+
+#include "ofxUISlider.h"
+
+class ofxUIFPSSlider : public ofxUISlider
+{
+public:        
+    ofxUIFPSSlider(float x, float y, float w, float h, float _min, float _max, float _value, string _name) : ofxUISlider(x, y, w, h, _min, _max, _value, _name)
+    {
+    }
+    
+    ofxUIFPSSlider(float w, float h, float _min, float _max, float _value, string _name) : ofxUISlider(w, h, _min, _max, _value, _name)
+    {
+        
+    }    
+    
+	void update()
+	{
+        setValue(ofGetFrameRate());   
+	}
+	
+}; 
+
+#endif
