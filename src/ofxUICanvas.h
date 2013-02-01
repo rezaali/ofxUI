87a9f3ee368e586157937e970a79941393d42cf5
rezaali <syed.reza.ali@gmail.com>
2013-02-01 15:56:48 -0800

    modified ofxUICanvas and got rid of #define OF_VERSION7

--- src/ofxUICanvas.h
+++ src/ofxUICanvas.h
@@ -561,83 +561,47 @@ public:
 	//App Callbacks
     void enableAppEventCallbacks()
     {
-#if OF_VERSION_MINOR >= 7 && OF_VERSION_PATCH > 0
         ofAddListener(ofEvents().update, this, &ofxUICanvas::onUpdate);
         ofAddListener(ofEvents().draw, this, &ofxUICanvas::onDraw);
         ofAddListener(ofEvents().exit, this, &ofxUICanvas::onExit);
-#else
-        ofAddListener(ofEvents.update, this, &ofxUICanvas::onUpdate);
-        ofAddListener(ofEvents.draw, this, &ofxUICanvas::onDraw);
-        ofAddListener(ofEvents.exit, this, &ofxUICanvas::onExit);
-#endif
     }
 	
 	//App Callbacks
     void disableAppEventCallbacks()
     {
-#if OF_VERSION_MINOR >= 7 && OF_VERSION_PATCH > 0
         ofRemoveListener(ofEvents().update, this, &ofxUICanvas::onUpdate);
         ofRemoveListener(ofEvents().draw, this, &ofxUICanvas::onDraw);
         ofRemoveListener(ofEvents().exit, this, &ofxUICanvas::onExit);
-#else
-        ofRemoveListener(ofEvents.update, this, &ofxUICanvas::onUpdate);
-        ofRemoveListener(ofEvents.draw, this, &ofxUICanvas::onDraw);
-        ofRemoveListener(ofEvents.exit, this, &ofxUICanvas::onExit);
-#endif
     }
 	
     void enableAppDrawCallback()
     {
-#if OF_VERSION_MINOR >= 7 && OF_VERSION_PATCH > 0
         ofAddListener(ofEvents().draw, this, &ofxUICanvas::onDraw);
-#else
-        ofAddListener(ofEvents.draw, this, &ofxUICanvas::onDraw);
-#endif    
     }
     
     void enableAppUpdateCallback()
     {
-#if OF_VERSION_MINOR >= 7 && OF_VERSION_PATCH > 0
         ofAddListener(ofEvents().update, this, &ofxUICanvas::onUpdate);
-#else
-        ofAddListener(ofEvents.update, this, &ofxUICanvas::onUpdate);
-#endif
     }
     
     void enableAppExitCallback()
     {
-#if OF_VERSION_MINOR >= 7 && OF_VERSION_PATCH > 0
         ofAddListener(ofEvents().exit, this, &ofxUICanvas::onExit);
-#else
-        ofAddListener(ofEvents.exit, this, &ofxUICanvas::onExit);
-#endif
     }
 
     void disableAppDrawCallback()
     {
-#if OF_VERSION_MINOR >= 7 && OF_VERSION_PATCH > 0
         ofRemoveListener(ofEvents().draw, this, &ofxUICanvas::onDraw);
-#else
-        ofRemoveListener(ofEvents.draw, this, &ofxUICanvas::onDraw);
-#endif
     }
     
     void disableAppUpdateCallback()
     {
-#if OF_VERSION_MINOR >= 7 && OF_VERSION_PATCH > 0
         ofRemoveListener(ofEvents().update, this, &ofxUICanvas::onUpdate);
-#else
-        ofRemoveListener(ofEvents.update, this, &ofxUICanvas::onUpdate);
-#endif
     }
     
     void disableAppExitCallback()
     {
-#if OF_VERSION_MINOR >= 7 && OF_VERSION_PATCH > 0
         ofRemoveListener(ofEvents().exit, this, &ofxUICanvas::onExit);
-#else
-        ofRemoveListener(ofEvents.exit, this, &ofxUICanvas::onExit);
-#endif
     }
 
 #ifdef TARGET_OPENGLES
@@ -645,36 +609,21 @@ public:
 	//Touch Callbacks
     void enableTouchEventCallbacks()
     {
-#if OF_VERSION_MINOR >= 7 && OF_VERSION_PATCH > 0
+
         ofAddListener(ofEvents().touchUp, this, &ofxUICanvas::onTouchUp);
         ofAddListener(ofEvents().touchDown, this, &ofxUICanvas::onTouchDown);
         ofAddListener(ofEvents().touchMoved, this, &ofxUICanvas::onTouchMoved);
         ofAddListener(ofEvents().touchCancelled, this, &ofxUICanvas::onTouchCancelled);
         ofAddListener(ofEvents().touchDoubleTap, this, &ofxUICanvas::onTouchDoubleTap);
-#else
-        ofAddListener(ofEvents.touchUp, this, &ofxUICanvas::onTouchUp);
-        ofAddListener(ofEvents.touchDown, this, &ofxUICanvas::onTouchDown);
-        ofAddListener(ofEvents.touchMoved, this, &ofxUICanvas::onTouchMoved);
-        ofAddListener(ofEvents.touchCancelled, this, &ofxUICanvas::onTouchCancelled);
-        ofAddListener(ofEvents.touchDoubleTap, this, &ofxUICanvas::onTouchDoubleTap);
-#endif
     }	
 
 	void disableTouchEventCallbacks()
     {
-#if OF_VERSION_MINOR >= 7 && OF_VERSION_PATCH > 0
         ofRemoveListener(ofEvents().touchUp, this, &ofxUICanvas::onTouchUp);
         ofRemoveListener(ofEvents().touchDown, this, &ofxUICanvas::onTouchDown);
         ofRemoveListener(ofEvents().touchMoved, this, &ofxUICanvas::onTouchMoved);
         ofRemoveListener(ofEvents().touchCancelled, this, &ofxUICanvas::onTouchCancelled);
         ofRemoveListener(ofEvents().touchDoubleTap, this, &ofxUICanvas::onTouchDoubleTap);
-#else
-        ofRemoveListener(ofEvents.touchUp, this, &ofxUICanvas::onTouchUp);
-        ofRemoveListener(ofEvents.touchDown, this, &ofxUICanvas::onTouchDown);
-        ofRemoveListener(ofEvents.touchMoved, this, &ofxUICanvas::onTouchMoved);
-        ofRemoveListener(ofEvents.touchCancelled, this, &ofxUICanvas::onTouchCancelled);
-        ofRemoveListener(ofEvents.touchDoubleTap, this, &ofxUICanvas::onTouchDoubleTap);
-#endif
     }	
 	
 #else
@@ -682,53 +631,31 @@ public:
 	//Mouse Callbacks
     void enableMouseEventCallbacks()
     {
-#if OF_VERSION_MINOR >= 7 && OF_VERSION_PATCH > 0
         ofAddListener(ofEvents().mouseReleased, this, &ofxUICanvas::onMouseReleased);
         ofAddListener(ofEvents().mousePressed, this, &ofxUICanvas::onMousePressed);
         ofAddListener(ofEvents().mouseMoved, this, &ofxUICanvas::onMouseMoved);
         ofAddListener(ofEvents().mouseDragged, this, &ofxUICanvas::onMouseDragged);
-#else
-        ofAddListener(ofEvents.mouseReleased, this, &ofxUICanvas::onMouseReleased);
-        ofAddListener(ofEvents.mousePressed, this, &ofxUICanvas::onMousePressed);
-        ofAddListener(ofEvents.mouseMoved, this, &ofxUICanvas::onMouseMoved);
-        ofAddListener(ofEvents.mouseDragged, this, &ofxUICanvas::onMouseDragged);
-#endif
     }
 
 	//Mouse Callbacks
     void disableMouseEventCallbacks()
     {
-#if OF_VERSION_MINOR >= 7 && OF_VERSION_PATCH > 0
         ofRemoveListener(ofEvents().mouseReleased, this, &ofxUICanvas::onMouseReleased);
         ofRemoveListener(ofEvents().mousePressed, this, &ofxUICanvas::onMousePressed);
         ofRemoveListener(ofEvents().mouseMoved, this, &ofxUICanvas::onMouseMoved);
         ofRemoveListener(ofEvents().mouseDragged, this, &ofxUICanvas::onMouseDragged);
-#else
-        ofRemoveListener(ofEvents.mouseReleased, this, &ofxUICanvas::onMouseReleased);
-        ofRemoveListener(ofEvents.mousePressed, this, &ofxUICanvas::onMousePressed);
-        ofRemoveListener(ofEvents.mouseMoved, this, &ofxUICanvas::onMouseMoved);
-        ofRemoveListener(ofEvents.mouseDragged, this, &ofxUICanvas::onMouseDragged);
-#endif
     }
 
     //Window Resize Callback
     void enableWindowEventCallbacks()
     {
-#if OF_VERSION_MINOR >= 7 && OF_VERSION_PATCH > 0
         ofAddListener(ofEvents().windowResized, this, &ofxUICanvas::onWindowResized);        
-#else
-        ofAddListener(ofEvents.windowResized, this, &ofxUICanvas::onWindowResized);        
-#endif
     }
 
 	//Window Resize Callback
     void disableWindowEventCallbacks()
     {
-#if OF_VERSION_MINOR >= 7 && OF_VERSION_PATCH > 0
         ofRemoveListener(ofEvents().windowResized, this, &ofxUICanvas::onWindowResized);        
-#else
-        ofRemoveListener(ofEvents.windowResized, this, &ofxUICanvas::onWindowResized);        
-#endif
     }
 	
 	
@@ -737,25 +664,15 @@ public:
     //KeyBoard Callbacks
 	void enableKeyEventCallbacks()
 	{
-#if OF_VERSION_MINOR >= 7 && OF_VERSION_PATCH > 0
 		ofAddListener(ofEvents().keyPressed, this, &ofxUICanvas::onKeyPressed);
 		ofAddListener(ofEvents().keyReleased, this, &ofxUICanvas::onKeyReleased);
-#else
-		ofAddListener(ofEvents.keyPressed, this, &ofxUICanvas::onKeyPressed);
-		ofAddListener(ofEvents.keyReleased, this, &ofxUICanvas::onKeyReleased);
-#endif
 	}
 
 	//KeyBoard Callbacks
 	void disableKeyEventCallbacks()
 	{
-#if OF_VERSION_MINOR >= 7 && OF_VERSION_PATCH > 0
 		ofRemoveListener(ofEvents().keyPressed, this, &ofxUICanvas::onKeyPressed);
 		ofRemoveListener(ofEvents().keyReleased, this, &ofxUICanvas::onKeyReleased);
-#else
-		ofRemoveListener(ofEvents.keyPressed, this, &ofxUICanvas::onKeyPressed);
-		ofRemoveListener(ofEvents.keyReleased, this, &ofxUICanvas::onKeyReleased);
-#endif
 	}
 
     void onUpdate(ofEventArgs &data) { if(autoUpdate) update(); }
